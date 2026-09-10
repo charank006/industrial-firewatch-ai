9b26ad781c9621d006c07d974b3d1200cd5ce9f1
Aniruth Reddy <aniruthreddylucky@gmail.com>
2026-09-10 10:32:36 +0530

    fix(frontend): bundle MapLibre worker using Vite worker import

--- frontend/src/components/map/GISMapLibre.tsx
+++ frontend/src/components/map/GISMapLibre.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useRef, useCallback, useState } from 'react';
 import * as maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import maplibreWorkerUrl from 'maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url';
+
+maplibregl.setWorkerUrl(maplibreWorkerUrl);
 import { classColorMatchExpression } from '../../utils/classColors';
 import { useIntelligence } from '../../context/IntelligenceContext';
 import { formatDisplayClassification } from '../intelligence/SituationRail';
